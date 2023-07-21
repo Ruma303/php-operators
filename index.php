9f4ad404e526feb31afd4571d06ffddc8fb46c33
Ruma303 <matteoh8@gmail.com>
2023-07-21 10:44:02 +0200

    aggiornamenti

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Live coding: php-operators</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Live coding: php-operators</title>
+
 <body>
     <h1 style="text-align: center">Operatori in PHP</h1>
-
     <h2>Operatori Aritmetici</h2>
     <?php
-        echo 2+2 . '<br>'; // 4
-        echo 8-3 . '<br>'; // 5
-        echo 4*4 . '<br>'; // 16
-        echo 25/5 . '<br>'; // 5
-        echo 10%3 . '<br>'; // 1
-        echo 3**3 . '<br>'; // 27
+        echo 2 + 2 . '<br>'; // 4
+        echo 8 - 3 . '<br>'; // 5
+        echo 4 * 4 . '<br>'; // 16
+        echo 25 / 5 . '<br>'; // 5
+        echo 10 % 3 . '<br>'; // 1
+        echo 3 ** 3 . '<br>'; // 27
     ?>
     <hr>
     <h2>Operatori di Stringhe</h2>
     <?php
-        echo 'Stringa 1' . ' + '. 'Stringa 2' . '<br>';
+        echo 'Stringa 1' . ' + ' . 'Stringa 2' . '<br>';
         $str1 = 'Stringa 1';
         echo $str1 .= ' + Stringa 2';
         // Entrambi: Stringa 1 + Stringa 2
@@ -34,12 +35,12 @@
         echo $var *= 2; // 24
         echo $var /= 4; // 6
         echo $var **= 2; // 36
-        echo $var %=4; // 0
+        echo $var %= 4; // 0
     ?>
     <hr>
     <h2>Operatori di Incremento/Decremento</h2>
     <?php
-        $x = 1;
+    $x = 1;
         echo ++$x; // 2
         echo $x++; // 2
         echo $x; // 3
@@ -53,7 +54,7 @@
         echo 3 < 2;
         echo 5 > 3; // 1
         echo 10 <= 10; // 1
-        echo 20 >= 2;  // 1
+        echo 20 >= 2; // 1
         echo 3 == '3'; // 1
         echo "Ciao" <> "3"; // 1
         echo false != false;
@@ -72,6 +73,14 @@
         $x = 3;
         echo !$x; // falso
     ?>
+
+    <hr>
+    <h2>Operatore di fusione NULL</h2>
+    <?php
+        $x = NULL;
+        $y = "Ciao, Mondo!";
+        //$x = "Ciao a tutti";
+        echo $x ?? $y; // "Ciao a tutti"
+    ?>
 </body>
 </html>
-
